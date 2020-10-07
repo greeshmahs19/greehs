0fa4d5fdf39f0c3ca877e1153757060e7689038e
greeshmahs19 <71968799+greeshmahs19@users.noreply.github.com>
2020-10-07 23:31:50 +0530

    Add files via upload

---
+++ day of the week.py
@@ -0,0 +1,22 @@
+#day of the week
+
+
+day = int(input('enter the number range of 1 through 7:'))
+if day == 1:
+    print('monday')
+elif day == 2:
+    print('tuesday')
+elif day == 3:
+    print('wednesday')
+elif day == 4:
+    print('thursday')
+elif day == 5:
+    print('friday')
+elif day == 6:
+    print('friday')
+elif day == 7:
+    print('sunday')
+else:
+    print('error message')
+        
+                        
---
+++ personal_info.py
@@ -0,0 +1,7 @@
+#personal information chapter 2
+print("name: greeshma hs")
+print("address: parimalanagar, bengaluru", \
+      "karnataka, 562125")
+print("telephone: 2315456")
+print("college major: business analytics")
+                
